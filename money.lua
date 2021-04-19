3148a39d389b6c360fe3150aac6c29588b01dbf6
Heygeon <heygeonng@gmail.com>
2021-04-19 15:40:16 +0800

    moneyAPI

--- money.lua
+++ money.lua
@@ -1,15 +1,10 @@
 os.loadAPI("encryptor")
 
-secure = fs.open("/disk/securefile", "r")
-encmoney = secure.readAll()
-secure.close()
-realmoney = encryptor.decrypt(encmoney, "vFhKE@k!U4F>xZXR")
-
 function setMoney(money)
-    securemoney = encryptor.encrypt(money,"vFhKE@k!U4F>xZXR")
+    local securemoney = encryptor.encrypt(money,"vFhKE@k!U4F>xZXR")
     --make and write securemoney file
     fs.delete("securefile")
-    h = fs.open("securefile", "w")
+    local h = fs.open("securefile", "w")
     h.write(securemoney)
     h.close()
     --copy it to the disk
@@ -17,3 +12,10 @@ function setMoney(money)
     fs.copy("securefile", "/disk/securefile")
 end
 
+function checkBalance()
+    local secure = fs.open("/disk/securefile", "r")
+    local encmoney = secure.readAll()
+    secure.close()
+    local realmoney = encryptor.decrypt(encmoney, "vFhKE@k!U4F>xZXR")
+    return realmoney
+end
